24102f12890c32f48d43c6cfcd043a87393c0173
Camden Bickel <cam@bickel.io>
2017-03-24 18:04:23 -0700

    Added template files, and gitignore

---
+++ hangman.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+from editme import main
+
+main()
+
+# pygame.init()
+# screen = pygame.display.set_mode((400, 300))
+# done = False
+# while 1:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:  # defined in pygame.locals
+#             pygame.quit()
+#             sys.exit()
+
+#     # Do other important game loop stuff here.
